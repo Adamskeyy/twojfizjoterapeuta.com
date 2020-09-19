12c48959413adabb6aa5ad2b9c3495cbed4c4931
Adam Antkowiak <adam.antkowiak93@gmail.com>
2020-09-19 12:44:08 +0200

    Correct scroll behavior, appropriate image sizing, hero section call to action added

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-
-
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-link');
 
-
     burger.addEventListener('click', () => {
-        // Toggle Nav
         nav.classList.toggle('nav-active');
-
-        // Animate Links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
@@ -18,13 +12,27 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 0.2}s`;
             }
         });
-        // Burger Animation
         burger.classList.toggle('toggle');
+    });
+}
+
+const setProperScrollMargin = () => {
+    const navHeight = document.querySelector('nav').offsetHeight;
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.style = `scroll-margin-top: ${navHeight}px;`
+    });
+}
+
+const adjustToResize = () => {
+    window.addEventListener('resize', () => {
+        setProperScrollMargin();
     });
 }
 
 const app = () => {
     navSlide();
+    adjustToResize();
 }
 
 app();
