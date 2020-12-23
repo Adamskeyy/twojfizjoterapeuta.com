f85f34ba438d958692020b436ef787a9453a146e
Adam Antkowiak <adam.antkowiak93@gmail.com>
2020-12-23 19:14:23 +0100

    Run app when DOM Content loaded only

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-link');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-link");
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 0.2}s`;
-            }
-        });
-        burger.classList.toggle('toggle');
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 6 + 0.2
+        }s`;
+      }
     });
-}
+    burger.classList.toggle("toggle");
+  });
+};
 
 const setProperScrollMargin = () => {
-    const navHeight = document.querySelector('nav').offsetHeight;
-    const sections = document.querySelectorAll('section');
-    sections.forEach(section => {
-        section.style = `scroll-margin-top: ${navHeight}px;`
-    });
-}
+  const navHeight = document.querySelector("nav").offsetHeight;
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    section.style = `scroll-margin-top: ${navHeight}px;`;
+  });
+};
 
 const adjustToResize = () => {
-    window.addEventListener('resize', () => {
-        setProperScrollMargin();
-    });
-}
+  window.addEventListener("resize", () => {
+    setProperScrollMargin();
+  });
+};
 
 const app = () => {
-    setProperScrollMargin();
-    navSlide();
-    adjustToResize();
-}
+  setProperScrollMargin();
+  navSlide();
+  adjustToResize();
+};
 
-app();
+document.addEventListener("DOMContentLoaded", app());
